f1469be3135962f13ad403a815b38e88f8b4150d
Owen Johnson <opjoh@gmail.com>
2024-04-02 21:20:07 -0500

    added route file structure

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes')
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
