072e3ac2d7d653f78db13d54e7a1b7768ceac766
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-04-03 19:46:35 +0000

    added code making sure users stay logged in and one for logging out. and added more comments.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -31,20 +31,23 @@ router.post('/', validateUserInput, async (req, res) => {
     // Error if user creation fails
   } catch (err) {
     console.error('Error creating user:', err);
-    res.status(400).json(err); 
+    res.status(400).json(err);
   }
 });
 
+// Finding user by email
 router.post('/login', validateUserInput, async (req, res) => {
   try {
     console.log('Logging in user...');
     const userData = await User.findOne({ where: { email: req.body.email } });
 
+    // Error response for user not found
     if (!userData) {
       console.log('User not found');
       return res.status(400).json({ message: 'Incorrect email or password, try again!' });
     }
 
+    // Checking if password matches users password
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
@@ -52,5 +55,35 @@ router.post('/login', validateUserInput, async (req, res) => {
       return res.status(400).json({ message: 'Incorrect email or password, try again!' });
     }
 
+    // Saving user ID and set logged-in for game
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      console.log('User logged in successfully');
+      // Respond with user data and success message
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    console.error('Error logging in user:', err);
+    // Error response if login fails
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    console.log('Logging out user...');
+    // Stop session and logs out the user
+    req.session.destroy(() => {
+      console.log('User logged out successfully');
+      res.status(204).end();
+    });
+  } else {
+    console.log('User not logged in');
+    // Error response if user is not logged in
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
