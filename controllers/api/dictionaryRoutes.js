37774c8f4dd5b231916e798a1046819012ccb7d3
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-04-04 01:14:10 +0000

    started code in dictionaryRoutes.js. the code defines a route to retrieve all entries from dictionary

--- controllers/api/dictionaryRoutes.js
+++ controllers/api/dictionaryRoutes.js
@@ -1,3 +1,16 @@
-const router = require('express').Router()
+const express = require('express');
+const { Dictionary } = require('../../models')
 
+// Route getting all words in dictionary
+router.get('/',async (req, res) => {
+    try {
+        // Naming is hard
+        const dictionaryEntries = await Dictionary.findAll();
+        res.json(dictionaryEntries);
+    } catch (err) {
+        // Logging errors and error added error message
+        console.error('Error fetching dictonary:', err);
+        res.status(500).json({ message: 'Internal error'});
+    }
+});
 module.exports = router;
