fb85961aed18b436699b764b40498e82e8c42755
Joe <joeyndiebel@gmail.com>
2024-04-11 18:46:27 -0500

    user

--- public/login.js
+++ public/login.js
@@ -30,7 +30,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (email && password) {
-    const response = await fetch("/api/user", {
+    const response = await fetch("/api/user/signup", {
       method: "POST",
       body: JSON.stringify({email, password }),
       headers: { "Content-Type": "application/json" },
